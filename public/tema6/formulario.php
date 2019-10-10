6dd8d722aa0f2972b8aa82f775570cc164c59e45
root <algoal1994@gmail.com>
2019-10-10 15:08:16 +0000

    commit

--- public/tema6/formulario.php
+++ public/tema6/formulario.php
@@ -1,28 +1,23 @@
 <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
 	<p>
 		<label for="name">Nombre</label>
-		<input type="text" name="name" value="<?= isset($_POST['name']) ? $_POST['name']
-			:  '' ?>"/>		
+		<input type="text" name="name" value="<?= showField('name') ?>"> <?php showErrorsField('name', $errors) ?>
 	</p>
 	<p>
-		<label for="email">Correo electronico</label>
-		<input type="email" name="email" value="<?= isset($_POST['email']) ? $_POST['email']
-			:  '' ?>"/>
+		<label for="email">Correo Electrónico</label>
+		<input type="email" name="email" value="<?= showField('email') ?>"> <?php showErrorsField('email', $errors) ?>
 	</p>
 	<p>
-		<label for="password">Contraseña</label>
-		<input type="password" name="password" value="<?= isset($_POST['password']) ? $_POST['password']
-			:  '' ?>"/>
+		<label for="password1">Contraseña</label>
+		<input type="password" name="password1"> <?php showErrorsField('password', $errors) ?>
 	</p>
 	<p>
-		<label for="password2">Repita la contraseña</label>
-		<input type="password" name="password2" value="<?= isset($_POST['password2']) ? $_POST['password2']
-			:  '' ?>"/>
+		<label for="password2">Repite la contraseña</label>
+		<input type="password" name="password2">
 	</p>
 	<p>
 		<label>
 			<input type="submit" value="Enviar">
 		</label>
 	</p>
-
 </form>
--- public/tema6/index.php
+++ public/tema6/index.php
@@ -1,51 +1,52 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Tema 6</title>
+	<meta charset="UTF-8">
+	<title>Formularios Amigables</title>
 </head>
 <body>
 	<h1>Formularios Usables</h1>
-	
-	<?php	
-	function showErrors($errors){
-		echo '<ul>';
-		foreach ($errors as $error) {
-			echo '<li>' . $error . '</li>';
+	<?php 
+	ini_set('DISPLAY_ERRORS', 1);
+	$errors = [];
+	include 'funciones.php';
+
+	if ( ! $_POST ) { 
+		include 'formulario.php';
+	} else {
+		if ( ! isset($_POST['name'])) {
+			$errors['name'] = 'No he recibido el nombre';
+		} elseif (strlen($_POST['name']) < 3){
+			$errors['name'] = 'El nombre es demasiado corto';
+		} 
+		if ( ! isset($_POST['email'])) {
+			$errors['email'] = 'No he recibido el email';
+		} elseif (strlen($_POST['email']) < 6) {
+			$errors['email'] = 'El correo electrónico no es válido';
+		} 
+		if ( ! isset($_POST['password1']) || ! isset($_POST['password2'])) {
+			$errors['password'] = 'No he recibido ambas contraseñas';
+		} else {
+			if ( strlen($_POST['password1']) < 6) {
+				$errors['password'] = 'La contraseña debe tener al menos 6 caracteres';
+			} 
+			if ($_POST['password1'] != $_POST['password2']) {
+				$errors['password'] = 'Ambas contraseñas deben ser iguales';
+			} 
 		}
-		echo '</ul><br>';
-	}
+		
+		if ($errors) {
+			showErrors($errors);
+			include 'formulario.php';
+		?>
+			
+		<?php
+		} else {
+			echo 'Todo bien, pasamos a registrar ese usuario';
+		}
 
-	if (! $_POST) {
-				include 'formulario.php';
-			} else {  
-				$errors = [];
-				if (! isset($_POST['name'])){
-					$errors[] = 'No he recibido el nombre';
-				} elseif (strlen($_POST['name'])<3){
-					$errors[] =  'El nombre es demasiado corto';
-				} 
-				if (! isset($_POST['email'])){
-					$errors[] =  'No se ha recibido el email';
-				} elseif (strlen($_POST['email']) < 6) {
-					$errors[] =  'El correo electronico no es valido';
-				} 
-				if (! isset($_POST['password']) || ! isset($_POST['password2'])) {
-					$errors[] = 'No he recibido ambas contraseñas';
-				} elseif (strlen($_POST['password'])< 6){
-					$errors[] = 'La contraseña debe tener al menos 6 caracteres';
-				} 
-				if ($_POST['password'] != $_POST['password2']) {
-					$errors[] = 'Ambas contraseñas deben ser iguales';
-				} 
+		include 'footer.php';
 
-				if ($errors) {
-					showErrors($errors);
-				}
-
-			include 'footer.php';
-
-			} ?>
-
+	} ?>
 </body>
 </html>
