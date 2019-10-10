7c1217de275287877e16bdca5fd1029c7c417a5b
root <algoal1994@gmail.com>
2019-10-10 14:18:58 +0000

    Commit inicial despues de varias clases

--- public/tema6/index.php
+++ public/tema6/index.php
@@ -7,24 +7,46 @@
 <body>
 	<h1>Formularios Usables</h1>
 	
-	<?php	if (! $_POST) {
+	<?php	
+	function showErrors($errors){
+		echo '<ul>';
+		foreach ($errors as $error) {
+			echo '<li>' . $error . '</li>';
+		}
+		echo '</ul><br>';
+	}
+
+	if (! $_POST) {
 				include 'formulario.php';
 			} else {  
+				$errors = [];
 				if (! isset($_POST['name'])){
-					echo 'No he recibido el nombre';
-				} elseif (strlen($_POST['name'])<3){
-					echo 'El nombre es demasiado corto';
-				} elseif (! isset($_POST['email'])){
-					echo 'No se ha recibido el email';
-				} elseif (strlen($_POST['email']) < 6) {
-					echo 'El correo electronico no es valido';
-				} elseif (! isset($_POST['password']) || ! isset($_POST['password2'])) {
-					echo 'No he recibido ambas contraseñas';
-				} elseif (strlen($_POST['password'])< 6){
-					echo 'La contraseña debe tener al menos 6 caracteres';
-				} elseif ($_POST['password'] != $_POST['password2']) {
-					echo 'Ambas contraseñas deben ser iguales';
-				} else {
+					$errors[] = 'No he recibido el nombre';
+				} 
+				if (strlen($_POST['name'])<3){
+					$errors[] =  'El nombre es demasiado corto';
+				} 
+				if (! isset($_POST['email'])){
+					$errors[] =  'No se ha recibido el email';
+				} 
+				if (strlen($_POST['email']) < 6) {
+					$errors[] =  'El correo electronico no es valido';
+				} 
+				if (! isset($_POST['password']) || ! isset($_POST['password2'])) {
+					$errors[] = 'No he recibido ambas contraseñas';
+				} 
+				if (strlen($_POST['password'])< 6){
+					$errors[] = 'La contraseña debe tener al menos 6 caracteres';
+				} 
+				if ($_POST['password'] != $_POST['password2']) {
+					$errors[] = 'Ambas contraseñas deben ser iguales';
+				} 
+				if ($errors) {
+					showErrors($errors);
+					echo '<br/><a href="index.php">Volver al formulario';
+				
+				}
+				else {
 					echo 'Todo bien, pasamos a registrar ese usuario';
 				}
 
