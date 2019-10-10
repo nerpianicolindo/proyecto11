7cdbbaad15f3973b980f9af7b422bc47a807e141
root <algoal1994@gmail.com>
2019-10-10 17:30:10 +0000

    empezar poo php

---
+++ public/tema11/Enlace.php
@@ -0,0 +1,11 @@
+<?php 
+	public class Enlace {
+		private a;
+
+	}
+
+
+
+
+?>
+
---
+++ public/tema11/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tema 11</title>
+</head>
+<body>
+
+	<h1>Temas</h1>
+	<ul>
+	<li><a href="tema1"><p>Tema 1</p></a></li>
+	<li><a href="tema2/index.php"><p>Tema 2</p></a></li>
+	<li><a href="tema3/index.php"><p>Tema 3</p></a></li>
+	<li><a href="tema4/index.php"><p>Tema 4</p></a></li>
+	<li><a href="tema6/index.php"><p>Tema 6</p></a></li>
+
+	
+</ul>
+
+</body>
+</html>
