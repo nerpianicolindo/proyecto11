74b3d47948e4bdca6d14838d8dfa3b1a0081b147
root <algoal1994@gmail.com>
2019-10-10 14:43:18 +0000

    commit

--- public/tema6/formulario.php
+++ public/tema6/formulario.php
@@ -1,19 +1,23 @@
 <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
 	<p>
 		<label for="name">Nombre</label>
-		<input type="text" name="name"/>		
+		<input type="text" name="name" value="<?= isset($_POST['name']) ? $_POST['name']
+			:  '' ?>"/>		
 	</p>
 	<p>
 		<label for="email">Correo electronico</label>
-		<input type="email" name="email"/>
+		<input type="email" name="email" value="<?= isset($_POST['email']) ? $_POST['email']
+			:  '' ?>"/>
 	</p>
 	<p>
 		<label for="password">Contraseña</label>
-		<input type="password" name="password"/>
+		<input type="password" name="password" value="<?= isset($_POST['password']) ? $_POST['password']
+			:  '' ?>"/>
 	</p>
 	<p>
 		<label for="password2">Repita la contraseña</label>
-		<input type="password" name="password2"/>
+		<input type="password" name="password2" value="<?= isset($_POST['password2']) ? $_POST['password2']
+			:  '' ?>"/>
 	</p>
 	<p>
 		<label>
--- public/tema6/index.php
+++ public/tema6/index.php
@@ -22,32 +22,25 @@
 				$errors = [];
 				if (! isset($_POST['name'])){
 					$errors[] = 'No he recibido el nombre';
-				} 
-				if (strlen($_POST['name'])<3){
+				} elseif (strlen($_POST['name'])<3){
 					$errors[] =  'El nombre es demasiado corto';
 				} 
 				if (! isset($_POST['email'])){
 					$errors[] =  'No se ha recibido el email';
-				} 
-				if (strlen($_POST['email']) < 6) {
+				} elseif (strlen($_POST['email']) < 6) {
 					$errors[] =  'El correo electronico no es valido';
 				} 
 				if (! isset($_POST['password']) || ! isset($_POST['password2'])) {
 					$errors[] = 'No he recibido ambas contraseñas';
-				} 
-				if (strlen($_POST['password'])< 6){
+				} elseif (strlen($_POST['password'])< 6){
 					$errors[] = 'La contraseña debe tener al menos 6 caracteres';
 				} 
 				if ($_POST['password'] != $_POST['password2']) {
 					$errors[] = 'Ambas contraseñas deben ser iguales';
 				} 
+
 				if ($errors) {
 					showErrors($errors);
-					echo '<br/><a href="index.php">Volver al formulario';
-				
-				}
-				else {
-					echo 'Todo bien, pasamos a registrar ese usuario';
 				}
 
 			include 'footer.php';
