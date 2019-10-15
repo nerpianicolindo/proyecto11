8474aba4b9661c1f268af5071b2739332519741d
root <algoal1994@gmail.com>
2019-10-15 14:05:44 +0000

    modificacion 2 enlace

--- public/tema11/enlace.php
+++ public/tema11/enlace.php
@@ -11,7 +11,7 @@
 			public $etiqueta;
 			public $url;
 			public function mostrar() {
-				echo 'Este es un enlace a'.'<a href=https://' . $this->url . '>' . $this->etiqueta . '</a>';
+				echo 'Este es un enlace a ' . '<a href="https://' . $this->url. '">' . $this->etiqueta . ' </a>';
 			}
 		}
 
@@ -23,3 +23,41 @@
 	?>
 </body>
 </html>
+
+
+<!--
+<!DOCTYPE html>
+<html>
+<head lang="es">
+	<meta charset="utf-8">
+	<title>Enlace</title>
+</head>
+<body>
+	<?php 
+
+
+	/*class Enlace
+		{
+			private $etiqueta;
+			private $url;
+
+			public function __construct($texto, $url){
+				$this->texto = $texto;
+				$this->url = $url;
+			}
+			public function mostrar() {
+				$aux = 'Este es un enlace a' . '<a href=https://' . $this->url . '>' . $this->etiqueta . '</a>';
+				return $aux;
+			}
+		}
+
+		$aux = 'Marca';
+		$aux2 = 'www.marca.com';
+
+		$google = new Enlace($aux, $aux2);
+		$aux = $google->mostrar();
+		echo $aux;
+		include 'footer.php';
+	*/?>
+</body>
+</html> -->
